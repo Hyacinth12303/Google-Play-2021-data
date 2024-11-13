35fbf5e34c8b19e717ccc80e9bf2d959fc00b08c
Hyacinth12303 <47294038+Hyacinth12303@users.noreply.github.com>
2024-11-13 15:34:19 +0800

    Update ProjectProposal.py

--- ProjectProposal.py
+++ ProjectProposal.py
@@ -565,18 +565,18 @@ elif st.session_state.page_selection == "machine_learning":
     ]
     category_encoder = LabelEncoder()
     code333 = """
-    from sklearn.tree import DecisionTreeRegressor
-    
-    # 1. Set up features and target
-    X = df[['total ratings', '5 star ratings', 'categoryLabel']]
-    y = df['rank']
-    
-    # 2. Split data into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    # 3. Train the DecisionTreeRegressor
-    tree_model = DecisionTreeRegressor(random_state=42)
-    tree_model.fit(X_train, y_train)
+        from sklearn.tree import DecisionTreeRegressor
+        
+        # 1. Set up features and target
+        X = df[['total ratings', '5 star ratings', 'categoryLabel']]
+        y = df['rank']
+        
+        # 2. Split data into train and test sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        
+        # 3. Train the DecisionTreeRegressor
+        tree_model = DecisionTreeRegressor(random_state=42)
+        tree_model.fit(X_train, y_train)
     """
     st.code(code333, language='python')
 
